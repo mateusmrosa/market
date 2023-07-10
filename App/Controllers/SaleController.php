d81b843a2d394869540df751ad3a9a867d020a76
Mateus Martinez Rosa <mayeusitau.unibanco@gmail.com>
2023-07-10 09:28:31 -0300

    atualizando

--- App/Controllers/SaleController.php
+++ App/Controllers/SaleController.php
@@ -9,27 +9,6 @@ use App\Models\SaleModel;
 
 class SaleController
 {
-    // public function getAllProducts(Request $request, Response $response)
-    // {
-    //     $productModel = new ProductModel();
-    //     $products = $productModel->getAllProducts();
-
-    //     $data = array();
-    //     foreach ($products as $row) {
-    //         $entry = array(
-    //             'product_id' => $row['product_id'],
-    //             'product_name' => $row['product_name'],
-    //             'product_price' => $row['product_price'],
-    //             'prod_type_id' => $row['prod_type_id'],
-    //             'prod_type_name' => $row['prod_type_name'],
-    //             'prod_type_percentage' => $row['prod_type_percentage']
-    //         );
-    //         $data[] = $entry;
-    //     }
-    //     $response->getBody()->write(json_encode($data));
-    //     return $response->withHeader('Content-Type', 'application/json');
-    // }
-
     public function create(array $data)
     {
         $saleModel = new SaleModel();
